b8fc46d73eefe4019cf072ed1fa909658e5ee384
Klemen <50655512+Klemen2@users.noreply.github.com>
2025-02-27 14:04:30 +0100

    mobile fix

--- packages/desktop/src/default_icon.rs
+++ packages/desktop/src/default_icon.rs
@@ -1,5 +1,3 @@
-use crate::{menubar::DioxusMenuIcon, DioxusTrayIcon};
-
 // TODO only implemented for windows, needs implementation for other platforms
 
 pub trait DefaultIcon {
@@ -9,7 +7,10 @@ pub trait DefaultIcon {
 }
 
 #[cfg(not(target_os = "windows"))]
-static ICON: &'static [u8; N] = include_bytes!("./assets/default_icon.bin");
+static ICON: &'static [u8] = include_bytes!("./assets/default_icon.bin");
+
+#[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
+use crate::DioxusTrayIcon;
 
 #[cfg(any(target_os = "linux", target_os = "macos", target_os = "windows"))]
 impl DefaultIcon for DioxusTrayIcon {
@@ -25,6 +26,9 @@ impl DefaultIcon for DioxusTrayIcon {
         default.expect("image parse failed")
     }
 }
+
+#[cfg(not(any(target_os = "ios", target_os = "android")))]
+use crate::menubar::DioxusMenuIcon;
 
 #[cfg(not(any(target_os = "ios", target_os = "android")))]
 impl DefaultIcon for DioxusMenuIcon {
